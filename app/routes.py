22e4b45cd4e2a03b41d5f615e54ccaf465e8d699
TarasZhere <zhere97@gmail.com>
2023-03-15 17:15:13 +0100

    testing templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,13 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, AmicaBet!"
+    users = [
+        {'name':'tom'},
+        {'name':'tim'},
+        {'name':'jack'},
+        {'name':'bob'},
+    ]
+    return render_template('index.html', title="home", users=users)
