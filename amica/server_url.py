bc21db7cdc1baa7caeeebf19c7886a3752920bf7
TarasZhere <zhere97@gmail.com>
2023-05-11 16:19:30 -0400

    testing soon

--- amica/server_url.py
+++ amica/server_url.py
@@ -1,4 +1,7 @@
-SERVER_URL = "http://127.0.0.1:5000/"
+import os
+
+SERVER_URL = f"http://{os.environ['SERVER_API']}:80/"
+# SERVER_URL = "http://{ipaddress here}:80/"
 
 headers = {
     'Content-type': 'application/json',
--- startup.sh
+++ startup.sh
@@ -2,7 +2,6 @@
 
 apt-get update -y
 apt-get upgrade -y
-apt-get install -y wget
 apt-get install -y python3-pip
 apt-get install -y git
 
