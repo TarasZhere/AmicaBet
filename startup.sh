f51f6b264dddbe42540efe46923f8f00607ffceb
TarasZhere <zhere97@gmail.com>
2023-05-11 16:06:51 -0400

    startup for k8 added

--- startup.sh
+++ startup.sh
@@ -9,4 +9,6 @@ apt-get install -y git
 pip3 install --upgrade flask
 
 # code download
-git clone 
+git clone --branch serversplit https://github.com/TarasZhere/AmicaBet_Flask.git
+
+flask --app server run --port 80 --host 0.0.0.0 
